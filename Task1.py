8b2ea18b2acc639ec3b247326a2ea8e73348f710
Uriti Sampath Vinayaka Srinivas <uritisampath2005@gmail.com>
2025-11-05 15:09:37 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+no_1 = int(input("Enter the first number : "))
+no_2 = int(input("Enter the second number : "))
+
+Add = no_1 + no_2
+Sub = no_1 - no_2
+Mul = no_1 * no_2
+Div = no_1 / no_2
+print("Addition: ", Add)
+print("Subtraction: ", Sub)
+print("Multipication: ", Mul)
+print("Division ", Div)
